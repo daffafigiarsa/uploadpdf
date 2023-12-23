b999d86844ec347ce3bf6db106317590ed761188
dendiriki <dendirikirahmawan@gmail.com>
2023-12-23 18:15:28 +0700

    coba di rumah

--- view_file.php
+++ view_file.php
@@ -83,7 +83,7 @@ if (!file_exists($filePath)) {
             }
         } else {
             // Jika itu adalah file, tampilkan konten file
-            echo '<object width="100%" height="600px" type="application/pdf" data="'.$filePath.'#toolbar=0" id="pdf_content">
+            echo '<object width="100%" height="700px" type="application/pdf" data="'.$filePath.'#toolbar=0" id="pdf_content">
             <p>Document load was not successful.</p>
              </object>';
         }
@@ -96,14 +96,17 @@ if (!file_exists($filePath)) {
     <!-- Tambahkan script Bootstrap JS (Opsional) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
-        window.addEventListener('keydown', function (e) {
+    // Mendeteksi peristiwa tombol pintasan cetak
+    window.addEventListener('keydown', function (e) {
+        // Mendeteksi tombol pintasan cetak (Ctrl + P di Windows/Linux, Command + P di Mac)
         if ((e.ctrlKey || e.metaKey) && (e.key === 'p' || e.key === 'P')) {
+            // Mencegah aksi default (pencetakan)
             e.preventDefault();
+            // Menampilkan pesan peringatan
             alert('Printing is disabled');
             return false;
         }
-        });
-
-    </script>
+    });
+</script>
 </body>
 </html>
